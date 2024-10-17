32645f12b24fc6a5274d5ede75fe748327292998
vupadhayayx86 <vupadhayayx86@gmail.com>
2024-10-17 10:35:42 +0530

    initial commit basic setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App(){
+    return(
+       <>
+        <div>Bulk Email Sender!</div>
+        <form>
+            <h1>React File Uplaod</h1>
+            <input type="file" />
+            <button type="submit">Upload</button>
+        </form>
+       
+       </>
+    )
+}
+
+export default App;
